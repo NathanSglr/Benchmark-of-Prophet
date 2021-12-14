6bd256c6cf57cf9f05d3773246c6c2f9bde12a37
NathanSglr <nathan.sanglier@imt-atlantique.net>
2021-12-14 23:49:05 +0100

    Update Intro.py

--- StreamlitApp/OtherPages/Intro.py
+++ StreamlitApp/OtherPages/Intro.py
@@ -8,7 +8,7 @@ def app():
     st.write('- **_Tuteur Entreprise_**: Hervé Mignot')
     st.write('------------------------------------------------------------------------------------------------------------------------')
     c1, c2 = st.columns(2)
-    c1.image('https://github.com/NathanSglr/test/blob/e8d458b4c31d9fa77fff54eb44d86bcdd801b95c/StreamlitApp/Pictures/Equancy_logo.png')
+    c1.image("https://github.com/NathanSglr/test/blob/e8d458b4c31d9fa77fff54eb44d86bcdd801b95c/StreamlitApp/Pictures/Equancy_logo.png")
 
     c2.image('https://github.com/NathanSglr/test/blob/e8d458b4c31d9fa77fff54eb44d86bcdd801b95c/StreamlitApp/Pictures/IMTA_logo.png')
     st.write('------------------------------------------------------------------------------------------------------------------------')
